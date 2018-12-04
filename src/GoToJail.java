706f5dcc12c5ab50889567238b0e20718e22cb0e
basakbuga <basakbuga@marun.edu.tr>
2018-12-04 17:31:37 +0300

    squares of the monopoly are updated.

--- src/GoToJail.java
+++ src/GoToJail.java
@@ -5,6 +5,7 @@ public class GoToJail extends Square {
         super(nameOfSquare, 0, 0);
     }
 	 public  void doAction(Player player){
+		 System.out.println("Don't pass the starting point and collect 200 TL!");
+		 player.setLocation(player,10);
 	 }
-	 
 }
